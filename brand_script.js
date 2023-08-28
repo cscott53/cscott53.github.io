fc787a6db9410dbf026a44273073b8d2fd4f39df
cscott53 <127893200+cscott53@users.noreply.github.com>
2023-08-28 12:23:50 -0400

    Added brand idea page

---
+++ brand_script.js
@@ -0,0 +1,7 @@
+// Add Google Font with JavaScript
+document.body.append(
+    document.createRange().createContextualFragment(`
+    <link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap" rel="stylesheet" />
+    `)
+  );
+  
