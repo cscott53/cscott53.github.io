d92d8ffb99a6c93cae87471e6fbd49d8cab9b8ce
cscott53 <127893200+cscott53@users.noreply.github.com>
2023-09-15 11:09:35 -0400

    Update brand_script.js

--- brand_script.js
+++ brand_script.js
@@ -1,6 +1,6 @@
 function showClock() {
     let now = new Date();
-    let today = (now.getMonth() + 1) + '/' now.getDays() + '/' now.getYear();
+    let today = (now.getMonth() + 1) + '/' + now.getDays() + '/' + now.getYear();
     let hours = now.getHours();
     let mins = now.getMinutes();
     let time = (hours % 12 + (hours === 0 ? 12 : 0)) + ':' + (mins < 10 ? '0' : '') + minutes + (hours > 12 ? 'PM' : 'AM');
