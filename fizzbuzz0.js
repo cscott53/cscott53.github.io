016a4c8c34399d5557605bd6a550a3604c8c49aa
cscott53 <127893200+cscott53@users.noreply.github.com>
2023-09-18 11:25:40 -0400

    Added files via VS code

---
+++ fizzbuzz0.js
@@ -0,0 +1,17 @@
+document.getElementById('submit').onclick = () => {
+    let firstName = document.getElementById('first-name').value
+    let middleInitial = document.getElementById('middle-initial').value
+    let lastName = document.getElementById('last-name').value
+    document.getElementById('welcome').textContent = `Welcome to Scripts Galore, ${firstName} ${middleInitial}. ${lastName}`
+    let count = prompt(`How high do you want to count, ${firstName}?`)
+    for (let i = 1; i <= count; i++) {
+        let fizzbuzz = document.getElementById('fizzbuzz')
+        let status = ''
+        if (i % 2) {
+            status = 'odd'
+        } else {
+            status = 'even'
+        }
+        fizzbuzz.innerHTML += `<li class="fizzbuzz-list">(${i}) Foo bar - The number is ${status}</li>`
+    }
+}
