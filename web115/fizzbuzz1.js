50133971713bba2cfe911a569f23e9c909d2e0c4
cscott53 <127893200+cscott53@users.noreply.github.com>
2023-09-24 14:17:12 -0400

    Added fizzbuzz1.js

---
+++ web115/fizzbuzz1.js
@@ -0,0 +1,21 @@
+document.getElementById('submit').onclick = () => {
+    let firstName = document.getElementById('first-name').value
+    let middleInitial = document.getElementById('middle-initial').value
+    let lastName = document.getElementById('last-name').value
+    document.getElementById('welcome').textContent = `Welcome to Scripts Galore, ${firstName} ${middleInitial}. ${lastName}`
+    let count = 140
+    for (let i = 1; i <= count; i++) {
+        let fizzbuzz = document.getElementById('fizzbuzz')
+        let status = ''
+        if (!(i % 15)) {
+            status = 'fizzbuzz'
+        } else if (!(i % 5)) {
+            status = 'buzz'
+        } else if (!(i % 3)) {
+            status = 'fizz'
+        } else {
+            status = 'nothing'
+        }
+        fizzbuzz.innerHTML += `<li class="fizzbuzz-list">(${i}) ${(status == 'fizzbuzz' ? 'Scripts unwrapped' : i == 'fizz' ? 'Scripts' : i == 'buzz' ? 'Unwrapped' : 'JS')}</li>`
+    }
+}
