a13e8be9fd86b0ce0f855179dc0ab83edb035b9a
cscott53 <127893200+cscott53@users.noreply.github.com>
2023-10-09 16:49:26 -0400

    Updated index.js

--- web115/index.js
+++ web115/index.js
@@ -1,4 +1,4 @@
-let links = document.querySelectorAll('.links')
+let links = document.querySelectorAll('.links:not(.current)')
 links.forEach(ele => {
     ele.addEventListener('mouseenter',() => ele.style.backgroundColor = 'black')
     ele.addEventListener('mouseleave',() => ele.style.backgroundColor = '#477878')
