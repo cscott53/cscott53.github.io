e8d1b3e80405ad840a7b7a882f05cfdd16fc25f7
cscott53 <127893200+cscott53@users.noreply.github.com>
2023-10-25 16:34:15 -0400

    Updated index.js

--- web115/index.js
+++ web115/index.js
@@ -1,5 +1,19 @@
-let links = document.querySelectorAll('.links:not(.current)')
-links.forEach(ele => {
-    ele.addEventListener('mouseenter',() => ele.style.backgroundColor = 'black')
-    ele.addEventListener('mouseleave',() => ele.style.backgroundColor = '#477878')
-})
+let links = document.querySelectorAll('.links')
+let pageName = document.querySelector('.page-name').textContent
+switch(pageName) {
+    case 'Home':
+        links[0].classList.add("current");break
+    case 'Introduction':
+        links[1].classList.add("current");break
+    case 'Course Contract':
+        links[2].classList.add("current");break
+    case 'Scripts Galore':
+        links[3].classList.add("current");break
+    case 'Fizzbuzz 0':
+        links[4].classList.add("current");break
+    case 'Fizzbuzz 1':
+        links[5].classList.add("current");break
+    case 'Fizzbuzz 2':
+        links[6].classList.add("current");break
+}
+//link.classList.add("current")
