cdc0390d75c88d0ee5d42b65e4ddde8c1301b571
cscott53 <127893200+cscott53@users.noreply.github.com>
2023-10-28 19:37:41 -0400

    Updated index.html and index.js

--- web115/index.js
+++ web115/index.js
@@ -1,19 +1,24 @@
-let links = document.querySelectorAll('.links')
-let pageName = document.querySelector('.page-name').textContent
-switch(pageName) {
-    case 'Home':
-        links[0].classList.add("current");break
-    case 'Introduction':
-        links[1].classList.add("current");break
-    case 'Course Contract':
-        links[2].classList.add("current");break
-    case 'Scripts Galore':
-        links[3].classList.add("current");break
-    case 'Fizzbuzz 0':
-        links[4].classList.add("current");break
-    case 'Fizzbuzz 1':
-        links[5].classList.add("current");break
-    case 'Fizzbuzz 2':
-        links[6].classList.add("current");break
-}
-//link.classList.add("current")
+setTimeout(()=>{ /* 
+                  * waits until after my other script
+                  * adds in the links and content
+                  */
+    let links = document.querySelectorAll('.links')
+    let pageName = document.querySelector('.page-name').textContent
+    switch(pageName) {
+        case 'Home':
+            links[0].classList.add("current");break
+        case 'Introduction':
+            links[1].classList.add("current");break
+        case 'Course Contract':
+            links[2].classList.add("current");break
+        case 'Scripts Galore':
+            links[3].classList.add("current");break
+        case 'Fizzbuzz 0':
+            links[4].classList.add("current");break
+        case 'Fizzbuzz 1':
+            links[5].classList.add("current");break
+        case 'Fizzbuzz 2':
+            links[6].classList.add("current");break
+    }
+    
+},10)
